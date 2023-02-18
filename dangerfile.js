5142a5de71ea6cf618c201a7f72249a983056f41
tlylt <liu_yongliang@hotmail.com>
2023-02-18 16:58:25 +0800

    u

--- dangerfile.js
+++ dangerfile.js
@@ -3,7 +3,7 @@ const { danger, warn } = require('danger')
 const pr = danger.github.pr;
 
 // Missing commit message (the commitMessageTitle is followed by a blank line)
-const commitMessageRegex = /\*\*Proposed commit message: \(wrap lines at 72 characters\)\*\*[\r\n\f\v]\n{1}/;
+const commitMessageRegex = /\*\*Proposed commit message: \(wrap lines at 72 characters\)\*\*[\r\n\f\v]{2}<!--/;
 if (commitMessageRegex.test(pr.body)) {
   warn(`Maybe fill in the issue description (especially the proposed commit message)?`);
 }
