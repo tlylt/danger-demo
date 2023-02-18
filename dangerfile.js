caee1df2b5023216ace20ca753686938533e4970
tlylt <liu_yongliang@hotmail.com>
2023-02-18 15:50:23 +0800

    update

--- dangerfile.js
+++ dangerfile.js
@@ -1,9 +1,23 @@
-const {danger, warn} = require('danger')
+const { danger, warn } = require('danger')
 
-  
-// No PR is too small to include a description of why you made a change
-if (danger.github.pr.body.length < 10) {
-  warn('Please include a description of your PR changes.');
+const pr = danger.github.pr;
+
+const commitMessageTemplate = `**Proposed commit message: (wrap lines at 72 characters)**
+
+<!--
+  See this link for more info on how to write a good commit message:
+  https://se-education.org/guides/conventions/git.html
+
+  As best as possible, write a succinct commit title in 50 characters
+
+|---------This is the width of 50 chars----------|
+|-----------This is the width of 72 chars for your reference-----------|
+-->
+
+---
+`;
+
+// Missing commit message (Commit message remains the default one)
+if (pr.body.length < 10 || pr.body.includes(commitMessageTemplate)) {
+  warn('Thanks for the additions! Maybe fill in the issue description (especially the proposed commit message)?');
 }
-
-  
