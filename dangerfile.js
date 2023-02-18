55b5622e7b9013d276205fa7b591b0da760fe103
tlylt <liu_yongliang@hotmail.com>
2023-02-18 16:29:47 +0800

    update

--- dangerfile.js
+++ dangerfile.js
@@ -5,5 +5,5 @@ const pr = danger.github.pr;
 // Missing commit message (the commitMessageTitle is followed by a blank line)
 const commitMessageRegex = /\*\*Proposed commit message: \(wrap lines at 72 characters\)\*\*[\r\n\f\v]\n{1}/;
 if (commitMessageRegex.test(pr.body)) {
-  warn(`Thanks for the additions ${pr.user.login}\nMaybe fill in the issue description (especially the proposed commit message)?`);
+  warn(`Maybe fill in the issue description (especially the proposed commit message)?`);
 }
